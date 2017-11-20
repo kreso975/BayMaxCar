58d06f749cacdc61714ed884bc87819e02459f79
Kresimir Kokanovic <kresimir.kokanovic@gmail.com>
2017-11-20 23:37:18 +0100

    code style

--- SocketServer/SocketServer.php
+++ SocketServer/SocketServer.php
@@ -32,19 +32,20 @@ $BayMax->pwmInit(); // Initialize Omega2 PWM extension board
 $BayMax->pwmSetOnDelay( $throttle, 100, 0); //this should give full throttle
 
 
-class echoServer extends WebSocketServer {
+class echoServer extends WebSocketServer
+{
     //protected $maxBufferSize = 1048576; //1MB... overkill for an echo server, but potentially plausible for other applications.
 
-    public $y = 0;      // Store y value not to repeat same requests - Moving
-    public $x = 0;      // Store x value not to repeat same requests - Steering
+    public $y = 0;      // Store y value not to repeat same instructions - Moving
+    public $x = 0;      // Store x value not to repeat same instructions - Steering
 
-    protected function process ($user, $message)
+    protected function process ( $user, $message )
     {
-        global $BayMax,$y,$x, $throttle, $turnForward, $turnBack, $turnLeft, $turnRight;
+        global $BayMax, $y, $x, $throttle, $turnForward, $turnBack, $turnLeft, $turnRight;
         $response = 0;
 
-
-        //echo $message;
+        // Fetch $message;
+        // Format: <instruction>=<value>
         $instruction = explode( '=', $message );
 
         switch (trim($instruction[0]))
@@ -91,23 +92,22 @@ class echoServer extends WebSocketServer {
             {
                 if ( $x !== $instruction[1] )
                 {
-                    if ($instruction[1] == 0)
+                    if ( $instruction[1] == 0 )
                     {
-                        //$BayMax->pwmInit();
                         $BayMax->pwmSetOnDelay( $throttle, 100, 0);
-                        $BayMax->pwmSetOnDelay( $turnRight, $string, 0);
-                        $BayMax->pwmSetOnDelay( $turnLeft, $string, 0);
+                        $BayMax->pwmSetOnDelay( $turnRight, 0, 0);
+                        $BayMax->pwmSetOnDelay( $turnLeft, 0, 0);
 
                         echo $instruction[1] . "x ";
                         $x = $instruction[1];
                     }
-                    else if ($instruction[1] < 0)
+                    else if ( $instruction[1] < 0 )
                     {
                         $string = str_replace('-', '', $instruction[1]);
                         $x = $instruction[1];
                         echo $string . "x ";
 
-                        if ($string > 0 && $string < 100)
+                        if ( $string > 0 && $string < 100 )
                         {
                             //$BayMax->pwmInit();
                             $BayMax->pwmSetOnDelay( $throttle, 100, 0);
@@ -121,17 +121,14 @@ class echoServer extends WebSocketServer {
                             $BayMax->pwmSetOnDelay( $turnRight, $string, 0);
                         }
                     }
-                    else if ($instruction[1] > 0)
+                    else if ( $instruction[1] > 0 )
                     {
                         $string = $instruction[1];
                         echo $string."x ";
                         $x = $instruction[1];
 
-                        if ($string < 100)
+                        if ( $string < 100 )
                         {
-                            //$BayMax->pwmInit();
-                            //$BayMax->pwmSetOnDelay( $throttle, 100, 0 );
-                            // $string = 9 - $string;
                             $BayMax->pwmSetOnDelay( $turnRight, 0, 0 );
                             $BayMax->pwmSetOnDelay( $turnLeft, $string, 0 );
                         }
@@ -152,9 +149,8 @@ class echoServer extends WebSocketServer {
             {
                 if ( $y !== $instruction[1] )
                 {
-                    if ($instruction[1] == 0)
+                    if ( $instruction[1] == 0 )
                     {
-                        //$BayMax->pwmInit();
                         $BayMax->pwmSetOnDelay( $throttle, 100, 0);
                         $BayMax->pwmSetOnDelay( $turnBack, $string, 0);
                         $BayMax->pwmSetOnDelay( $turnForward, $string, 0 );
@@ -162,7 +158,7 @@ class echoServer extends WebSocketServer {
                         echo $instruction[1] . "y ";
                         $y = $instruction[1];
                     }
-                    else if ($instruction[1] < 0)
+                    else if ( $instruction[1] < 0 )
                     {
                         $string = str_replace('-', '', $instruction[1]);
                         $y = $instruction[1];
@@ -190,9 +186,7 @@ class echoServer extends WebSocketServer {
 
                         if ( $string < 100 )
                         {
-                            //$BayMax->pwmInit();
                             $BayMax->pwmSetOnDelay( $throttle, 100, 0 );
-                            // $string = 9 - $string;
                             $BayMax->pwmSetOnDelay($turnBack, 0, 0);
                             $BayMax->pwmSetOnDelay( $turnForward, $string, 0 );
                         }
@@ -215,13 +209,15 @@ class echoServer extends WebSocketServer {
         $this->send($user,$response);
     }
 
-    protected function connected ($user) {
+    protected function connected ($user)
+    {
         // Do nothing: This is just an echo server, there's no need to track the user.
         // However, if we did care about the users, we would probably have a cookie to
         // parse at this step, would be looking them up in permanent storage, etc.
     }
 
-    protected function closed ($user) {
+    protected function closed ($user)
+    {
         // Do nothing: This is where cleanup would go, in case the user had any sort of
         // open files or other objects associated with them.  This runs after the socket
         // has been closed, so there is no need to clean up the socket itself here.
